79c7ff1d0d5bfcbcedef202cf82b6d80c6e11888
office-itou <itou-jun@outlook.com>
2018-03-28 22:21:27 +0900

    update

--- customize/knoppix-live.sh
+++ customize/knoppix-live.sh
@@ -1,139 +1,158 @@
 #!/bin/bash
 #set -evx
 # *****************************************************************************
-# LiveCDCustomization [KNOPPIX_V8.1-2017-09-05-EN]                            *
+# LiveCDCustomization [KNOPPIX_V8.1-2017-09-05-EN.iso]                        *
 # *****************************************************************************
-	LIVE_VNUM=V8.1-2017-09-05
+	LIVE_FILE="KNOPPIX_V8.1-2017-09-05-EN.iso"
+	LIVE_DEST=`echo "${LIVE_FILE}" | sed -e 's/-EN/-JP/g'`
 # == initialize ===============================================================
 	trap 'exit 1' 1 2 3 15
 # == tools install ============================================================
-	apt-get -y install squashfs-tools genisoimage cloop-utils
+	apt-get -y install squashfs-tools xorriso cloop-utils
 # == initial processing =======================================================
-#	cd ~
-	rm -Rf   ./knoppix-live
+	rm -rf   ./knoppix-live
+#	rm -rf   ./knoppix-live/media ./knoppix-live/cdimg ./knoppix-live/fsimg
 	mkdir -p ./knoppix-live/media ./knoppix-live/cdimg ./knoppix-live/fsimg
-# -----------------------------------------------------------------------------
-#	tar -cz knoppix-setup.sh | xxd -ps
+	# -------------------------------------------------------------------------
+	#	tar -cz ./knoppix-setup.sh | xxd -ps
 	if [ ! -f ./knoppix-setup.sh ]; then
 		cat <<- _EOT_ | xxd -r -p | tar -xz
-			1f8b0800d8847f5a0003ed186b73db36325fa55fb1953d51e31b889212d7
-			39c5729bda8eebc64e5c3fd2cc45ad0622211116093000684b71dcdfde05
-			493d28cba9233b737373da198a8bc562b92f00bbea0b19457c4034337154
-			d1fea36f0055848d8d8de48d30f3ae3dadad6f3caa3dad3fddd8a8d6eb88
-			576bf58d1f9e3d82eab7506616626da80278a4a4345fe2fba7f9ff5158f9
-			cee970e174a8f68b2b980440d8c5a0b8028480612ae4821a8683fb41b12b
-			dc5de17dff04ae8a058719d7e1829b8ae7789d588336322a16e250c6c280
-			e3b10b27321a3e7f868c44bac1989ce3030b37f9263c7aa8e7f12079c213
-			29e9cee1b1e4e275ea08ab2ca701ff746f4f140b46d108caa93f5e001b70
-			03b532d4a00e4fa1b65e2cb041249581a3935ab3f4bdebdbac7b02a56221
-			53cdc048df0992ea3ae140f3ba89dd6324b578cc803e326184536327a66e
-			9b66b02180493426fe9f133eaa4ceaa8507a71c0d05f480b82fb380be56d
-			862c945b8800f8c644bae13801d746573cd6e15454a4ea39294a62cd9453
-			afd66a4ef59913ea1e623f3caff8260c72cb432a22da633909da2834c877
-			68641c2d63e5e2b4fd4c65bd724e27222cec44fd5ea3f136325c0add6834
-			d1beaec405c495a2ebb12e4895230976394b92813716f7d2fd1873c51a8d
-			6d9fb97df20e13cc2367c2f0a029e458efc46a110f88f54645d37eac6845
-			b0ca79e4f4d3a37bf46ebb7894c9f03caa3eafd6aad5649837c04303c8c7
-			98a92190df512fedcb4bd42da4a6595ebdda4fa3c63c7282997edd32ab57
-			47d4eda3c3ae5ba20c9f41dbc42442211a308da9a0990784439211b3fe83
-			79d02a16ec8b30286ba76b220cc4742c2ce53c9aa6f4cac5c2cba3d3f6f1
-			ee41b36453dbd00e2618628669c345cfa2b1989071fb30c54326d094d23f
-			accde6cf8e769aa5d5ab8cf51a88fc7268c6cbf66697cd26c84c7e640bf7
-			df9c2cb6f0f8f05db38438ba9af4ec49fd11c810324987efae218a558fcd
-			75fc2d90c5234db0347a64ed6b04cc11d673651829ae1796332d8c1939bc
-			a7a429cd840c19e9d190dd4764264cb00ba63ab85feea55d264cbb3ca09d
-			7b497afcd80acba7c614d0d8488527c8c59df2e386308434cf70b35c431c
-			79b62a5854b3071536de01b8adaec737cf9d210b000f93bdc67bc0f14a23
-			a1fce4de5dc6acb080771493dd2ec7dde4b32022e734470b6a5561697712
-			36bab3ec82299c8c6eeeafd20c0b053c63bd054c9b23acebb981145f75de
-			ccc2374fda19616ec0a8584cb33c24828a85e3ddd3ed9de6ea8fc502efc2
-			074cc48480473abaa50a7fbc00e333642ba4e51e2269bd572c74795a3005
-			d27d90bad2569651ac7d0f9c582b47fb54312711ce1cd84aab37110701aa
-			a04220aa0b1db6069dde1ab878167a141f1cdb73b1cbf1516be0e3d847dc
-			8fd7809b351078d045f8281ceb3e3e881be4fbe4afe1a7252655ee33e9b7
-			6d5144882d070855aecf31909fa1a75804e4c256017f6252db1713edbd9f
-			2d86c8d909f20ca8ea699816e2b1801946ba4a862359f90264e0f59c60a0
-			b130c14bd43978bfb3ebd89027d5e94cf4a6ca9055fa13363d837e27c4b2
-			9c0474286303e788cae43206d7a8a0a12f69e452cdca69d06c4d0eb6e42c
-			63f1cb4c5a862c1834971ad8dc24d0de7d7bda4617fee5542e78a8b09c2f
-			d8664cc46187a96c80f58b6d959acfb2b1f5eb14ea62d07513991a5bed62
-			22ef86f844be6df5920face063cd82526bf5ea74f7f8f0ba84072812ad73
-			4a4951508227d9d8c2c1cb377bcded29c28b17d960ed26df396dff7a5439
-			3b7d459ecfae609abac966485a1dcb3d19ed9dbe6eef1fb60fdfee9c1dec
-			36ed693c997b8fd4fd57fbbbc727cd9f783833f9dbe9ecbad40749c8b212
-			f901a2863ec5ec88c681c21ec9c7b2625c8c4fcda78ebe398f55b700723c
-			52aa32522ecf38ca83bbb267614d1bd6f175b6585e4e999bebc3b67d2ab0
-			c27c4dc539b7cd1e6b7cf89d0b4f5e6ae8b3e11ffffa701251972538d9fa
-			f01f26fad8af38bfd0e49d90f1a49ce826200d61d8f7b89ab567c0451463
-			938967099aafd34dfe1e5bbc816d3e9dd1b493e4d96d8bd3d9bb3b71fcd1
-			c48d6e40437a71ffb3d966cdf45195ca75ba8a69dfe215eb903b5d4bb740
-			aeaf5a81a46b016c21c0b6a0585ed18edde6f56760b035d2409136bc9daf
-			56cff1f5ca79f9c92a8dd24658ad6e791e4cff3fdf48c3bbc36d748ce7ac
-			3cb63d60120d6dafb70788c56c3450ae7dbc7696968b297d64ff2c33c778
-			351cc81e17505973664943a6e7382a1fbb23aaf5a554decbd8960e86e301
-			6eaf21bbf40b53f91039bfe0adf79a0da165ed6b59e3929fb68ff436733d
-			4ddbb8171d6f4ed0ee2ec6abafafd7fe7d8ba0dc0d7ba6d9ce9b1310b225
-			f6b107530c09eab5c01d69bfa071228bf0a83c7de808677247ef85b65bce
-			2238b5fb63eff014b0a46f89cdbd40622bb8d512ada9704b61c768eb2b13
-			598b7542d97432e6725e47542c4e98e6e4962d3870bb26afd16e482ad0c5
-			2b8f9caf46e5a98f5584b47f0eb9c5b452ada6dfda7dfbea61361e29feb7
-			ffee5ec21296b084252c61094b58c21296b084252ce1ff12fe06b707c476
+			1f8b0800e886bb5a0003ed586d73d33810ee57fb57e84a87c0dd28b6db42
+			990ce9c1d0003de0da6bcacb1cdc64145b8995d89291e436a5e47efbad6c
+			2771d200a1297377337ebe78bd921eaf7657da4deace908b246123aca84e
+			93ba0a376e1c2e606f77377b02169f7b7baeb7e1ed6cdfbbbfe7eeec78db
+			1baeb7e3b93b1bc8bd7953ae22559a488436a410fa6bf3be35fe3fc5ad9f
+			9c2ee34e97a8d0be052980303d1bd9b710c6485319334e348597f560f7b8
+			dfe2c19dbbe8d2b64e5aa79df669bbb9e5d9b61532a585bc40d8b72d876a
+			df619ce97ae004dd5421184a6c2b8d45ca3572027ae6245aa1cf9f51a1c2
+			bd68aa9e9b870caece9bcd51176ad91c50cfe62452f84be61835184e474c
+			a3adcb6233637b9cfbcc98cf48c43eaded34dbd29224a896bb0e7935e4a1
+			6db483bc7be6e389901a1db7bde6e61d3f34a979176dda5661a64613db67
+			426ef76c066cb597f9602ae4bb9f4e007fe93881a1a943731796279870a0
+			596466b158124a2275eea258046944c153a08ba275dc047c0f631a8b7d10
+			100ab54e54c371224829550f6897115e17b2efe4224e1595ceb6eb798ebb
+			ebc4aa0fd2fd07f550c7d1dcf298f084f4e91c83d21236143a24d18e12a9
+			f461d87ca67eaf3e20330a838364d86f348e12cd04578d4613f6d713b000
+			fb82f702da4342cea9383d5f54892898d23df63fa64cd246e34948fd217e
+			03a915e0d75cb3a8c9c5d4ee6cd73c1d61e38dba22c354923aa7f54132b9
+			dd27cf8e0ff79d880789fbc0f55c377b9ddf40001bc01f536a0ee55bb04b
+			85e21c6c8b896ed6b62e0ff3a8d100b721c7c71ff4d6e531f187e0b0f107
+			5e439f91328989b90431a20a5241d1006186b28c58f41f5a860fb6651e98
+			a29a727a3a81409463613483a4ace9d76cebf1f169e7a4f5b2b969525b93
+			2e2418489a2acd78df88299fa9e1f850c962ca612b9bf9dad7c707cdcdad
+			cb82668cb0f8baeba7cb9e2d2e5b4c8085f8170b0f7f6f5f6fe1c9ab37cd
+			4d90c195b86faeeb8fc65939136c628cd2243057f68ab87ddbf8bb44862f
+			0a32f8d01825a9ecafcc550a5e9e8d79a8f1cfdf43b084acef8b38914c5d
+			9ba74c46b5b85893a964191731c57d12d375280b324ecfa8ecc2e15acbba
+			824cf92c22ddb59896a546092485c20dd7cdd94af971850cfd90a4856335
+			9e56969551f84c1335c40392104e159d7fc3015543e84656276371767059
+			1f31a87f38169ffcd50d5a249bd424b0a72ce34965fe2eb288752515bd1e
+			83a319d22831a4655de4b9dce8be821f9e1a0b647e44095f698fdf205b9d
+			c8003a3f202b1ab0ad5ff3f62512fe8df477a6c3f389460f1f62d4691d9d
+			76d0fe7e5e254741df89460aaa275402e7e5bb8396639c90b75096f508ba
+			f4d1b01b437388237221528d06208aac70205fcba8a1ce49e21345ed8c38
+			b7db3489c8f44035e8c6a8ceebe2cdd88dfe76ea672c96d05f5ae627044f
+			e32e95c50b545dd3c737778b7753f74ba21f12a99a30a9b1df299b5bf42b
+			3760f155739d10aeed69677493a62f89e8fcb7ccefacec63450bffecf445
+			e7f055e7d5d1c1eb97ada6b92a6663ef407bf8f4b075d26e3e62f1c2e01f
+			a78beb4ace9b5d3ed78b70c979732df6939070e8075e103e60a68fa78df7
+			6f190fc4b942437af1d72fefdb09f16926e3fdf77f523e8456d4794eb267
+			a6b6ad926d1ce57b8a8701938b9e1a319ea4f0fbc1b6228eb0ca8fc63be8
+			de47e6778533197606a4f3dbf19716e7a31016686281e56492568d497a7d
+			e9a3991bb3dbe2fae7642e078b5bc4cd995b474fd727cd99ffed3f102a54
+			a850a142850a152a54a850a142850a152a54a850a1c27f12ff00f3ee7c49
 			00280000
 _EOT_
 	fi
-# -----------------------------------------------------------------------------
-	if [ ! -f ./KNOPPIX_${LIVE_VNUM}-EN.iso ]; then
-		wget "http://ftp.riken.jp/Linux/knoppix/knoppix-dvd/KNOPPIX_${LIVE_VNUM}-EN.iso"
-#		wget "http://ftp.kddilabs.jp/.017/Linux/packages/knoppix/knoppix-dvd/KNOPPIX_${LIVE_VNUM}-EN.iso"
+	# -------------------------------------------------------------------------
+	if [ ! -f ./${LIVE_FILE} ]; then
+		wget "http://ftp.riken.jp/Linux/knoppix/knoppix-dvd/${LIVE_FILE}"
+#		wget "http://ftp.kddilabs.jp/.017/Linux/packages/knoppix/knoppix-dvd/${LIVE_FILE}"
 	fi
-	LIVE_VOLID=`volname ./KNOPPIX_${LIVE_VNUM}-EN.iso`
-# -----------------------------------------------------------------------------
-	mount -o loop ./KNOPPIX_${LIVE_VNUM}-EN.iso ./knoppix-live/media
+	# -------------------------------------------------------------------------
+	LIVE_VOLID=`volname ./${LIVE_FILE}`
+	# -------------------------------------------------------------------------
+	ISO2_START=`fdisk -l ./${LIVE_FILE} | awk '$1=="'./${LIVE_FILE}2'" { print $2; }'`
+	ISO2_COUNT=`fdisk -l ./${LIVE_FILE} | awk '$1=="'./${LIVE_FILE}2'" { print $4; }'`
+	# -------------------------------------------------------------------------
+	dd if=./${LIVE_FILE} of=./knoppix-live/efiboot.img bs=512 skip=${ISO2_START} count=${ISO2_COUNT}
+	# -------------------------------------------------------------------------
+	mount -o loop ./${LIVE_FILE} ./knoppix-live/media
 	cp -rp ./knoppix-live/media/* ./knoppix-live/cdimg/
 	umount ./knoppix-live/media
-# -----------------------------------------------------------------------------
-	mv ./knoppix-live/cdimg/KNOPPIX/KNOPPIX ./knoppix-live/cdimg/KNOPPIX/KNOPPIX.orig
-	extract_compressed_fs ./knoppix-live/cdimg/KNOPPIX/KNOPPIX.orig ./knoppix-live/KNOPPIX_FS.iso
-# -----------------------------------------------------------------------------
+	# -------------------------------------------------------------------------
+	extract_compressed_fs ./knoppix-live/cdimg/KNOPPIX/KNOPPIX ./knoppix-live/KNOPPIX_FS.iso
+	rm ./knoppix-live/cdimg/KNOPPIX/KNOPPIX
+
 	mount -o loop ./knoppix-live/KNOPPIX_FS.iso ./knoppix-live/media
 	cp -rp ./knoppix-live/media/* ./knoppix-live/fsimg/
 	umount ./knoppix-live/media
-# -----------------------------------------------------------------------------
-	cp -p ./knoppix-setup.sh ./knoppix-live/fsimg/root
-	mv ./knoppix-live/fsimg/etc/localtime ./knoppix-live/fsimg/etc/localtime.orig
+# =============================================================================
+	rm ./knoppix-live/fsimg/etc/localtime
 	ln -s /usr/share/zoneinfo/Asia/Tokyo ./knoppix-live/fsimg/etc/localtime
-	sed -i ./knoppix-live/fsimg/etc/adjtime \
+	sed -i ./knoppix-live/fsimg/etc/adjtime               \
 	    -e 's/LOCAL/UTC/g'
 	sed -i ./knoppix-live/fsimg/etc/rc.local              \
 	    -e 's/^SERVICES="\([a-z]*\)"/SERVICES="\1 ssh"/g'
-# -----------------------------------------------------------------------------
-	LANG=C chroot ./knoppix-live/fsimg /bin/bash /root/knoppix-setup.sh
-	RETCD=$?
-	if [ ${RETCD} -ne 0 ]; then
-		exit ${RETCD}
-	fi
-# -----------------------------------------------------------------------------
+	# -------------------------------------------------------------------------
+	cp -p ./knoppix-setup.sh ./knoppix-live/fsimg/root
+	LANG=C chroot ./knoppix-live/fsimg /bin/bash /root/knoppix-setup.sh || exit $?
+	# -------------------------------------------------------------------------
 	rm -rf ./knoppix-live/fsimg/root/knoppix-setup.sh        \
 	       ./knoppix-live/fsimg/root/.bash_history           \
 	       ./knoppix-live/fsimg/root/.viminfo                \
 	       ./knoppix-live/fsimg/tmp/*                        \
 	       ./knoppix-live/fsimg/var/cache/apt/*.bin          \
 	       ./knoppix-live/fsimg/var/cache/apt/archives/*.deb
-# -- make iso image -----------------------------------------------------------
+# =============================================================================
 	sed -i ./knoppix-live/cdimg/boot/isolinux/isolinux.cfg \
 	    -e 's/lang=en/lang=ja/'                            \
 	    -e 's/^APPEND.*/\0 tz=Asia\/Tokyo/g'
+
+	sed -i ./knoppix-live/cdimg/boot/isolinux/syslnx32.cfg \
+	    -e 's/lang=en/lang=ja/'                            \
+	    -e 's/^APPEND.*/\0 tz=Asia\/Tokyo/g'
+
+	sed -i ./knoppix-live/cdimg/boot/isolinux/syslnx64.cfg \
+	    -e 's/lang=en/lang=ja/'                            \
+	    -e 's/^APPEND.*/\0 tz=Asia\/Tokyo/g'
+	# -------------------------------------------------------------------------
+	mount -o loop ./knoppix-live/efiboot.img ./knoppix-live/media
+
+	sed -i ./knoppix-live/media/boot/syslinux/syslnx32.cfg \
+	    -e 's/lang=en/lang=ja/'                            \
+	    -e 's/^APPEND.*/\0 tz=Asia\/Tokyo/g'
+
+	sed -i ./knoppix-live/media/boot/syslinux/syslnx64.cfg \
+	    -e 's/lang=en/lang=ja/'                            \
+	    -e 's/^APPEND.*/\0 tz=Asia\/Tokyo/g'
+
+	umount ./knoppix-live/media
+# =============================================================================
 	pushd ./knoppix-live/fsimg > /dev/null
-		genisoimage -D -R -U -V "KNOPPIX_FS" -o ../KNOPPIX_FS.tmp .
+		xorriso -as mkisofs \
+		    -D -R -U -V "KNOPPIX_FS" \
+		    -o ../KNOPPIX_FS.tmp \
+		    .
 		create_compressed_fs -B 131072 -f ../isotemp -q - ../cdimg/KNOPPIX/KNOPPIX < ../KNOPPIX_FS.tmp
 	popd > /dev/null
+	# -------------------------------------------------------------------------
+	cp ./knoppix-live/efiboot.img ./knoppix-live/cdimg/
 	pushd ./knoppix-live/cdimg > /dev/null
-		find KNOPPIX -name "KNOPPIX*" -not -name "KNOPPIX.orig" -type f -exec sha1sum -b {} \; > KNOPPIX/sha1sums
-		genisoimage -l -r -J -V "${LIVE_VOLID}" -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat \
-		    -no-emul-boot -boot-load-size 4 -boot-info-table -o ../../KNOPPIX_${LIVE_VNUM}-JP.iso -x KNOPPIX.orig -allow-limited-size .
-		# isohybrid ../../KNOPPIX_${LIVE_VNUM}-JP.iso
+		find KNOPPIX -name "KNOPPIX*" -type f -exec sha1sum -b {} \; > KNOPPIX/sha1sums
+		xorriso -as mkisofs \
+		    -D -R -U -V "${LIVE_VOLID}" \
+		    -o ../../${LIVE_DEST} \
+		    -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
+		    -b boot/isolinux/isolinux.bin \
+		    -c boot/isolinux/boot.cat \
+		    -no-emul-boot \
+		    -boot-load-size 4 \
+		    -boot-info-table \
+		    -iso-level 4 \
+		    -eltorito-alt-boot -e efiboot.img -no-emul-boot \
+		    .
 	popd > /dev/null
-	ls -lh ./knoppix-live/cdimg/KNOPPIX/KNOPPIX*
-	ls -lh ./KNOPPIX*.iso
+# =============================================================================
 	exit 0
 # == EOF ======================================================================
--- customize/knoppix-setup.sh
+++ customize/knoppix-setup.sh
@@ -2,14 +2,19 @@
 #set -evx
 # -- terminate ----------------------------------------------------------------
 fncEnd() {
+	RET_STS=$1
+
+	history -c
 	/etc/init.d/dbus stop
 	umount /dev/pts || umount -fl /dev/pts
 	umount /dev     || umount -fl /dev
 	umount /sys     || umount -fl /sys
 	umount /proc    || umount -fl /proc
+
+	exit ${RET_STS}
 }
 # -- initialize ---------------------------------------------------------------
-	trap 'fncEnd; exit 1' 1 2 3 15
+	trap 'fncEnd 1' 1 2 3 15
 	export PS1="(chroot) "
 	mount -t proc     proc     /proc
 	mount -t sysfs    sysfs    /sys
@@ -27,11 +32,11 @@ fncEnd() {
 	sed -i /etc/apt/sources.list                     \
 	    -e 's/ftp.de.debian.org/ftp.jp.debian.org/g'
 	APT_REL="-t stable -t testing -t unstable -t experimental"
-	APT_REL="-t stable -t testing"
 	APT_UPD="${APT_REL} -o Acquire::Check-Valid-Until=no"
 	APT_UPG="${APT_REL} -o Dpkg::Options::=--force-confdef"
 	APT_INS="${APT_REL} -o Dpkg::Options::=--force-confdef"
 	APT_RMV=""
+	apt-get -q    ${APT_UPD} update                                        && \
 	apt-get -q -y ${APT_RMV} purge                                            \
 	    linux-source-*                                                        \
 	    gcompris*                                                             \
@@ -41,28 +46,19 @@ fncEnd() {
 	    scilab*                                                            && \
 	apt-get -q -y            autoremove                                    && \
 	apt-get -q    ${APT_UPD} update                                        && \
-	apt-get -q    ${APT_UPD} update                                        && \
 	apt-get -q -y ${APT_INS} install                                          \
+	    task-japanese task-japanese-desktop                                   \
 	    im-config ibus-mozc                                                   \
-	    libreoffice-help-ja libreoffice-l10n-ja                               \
 	    manpages-ja manpages-ja-dev                                           \
-	    proftpd                                                               \
-	    fdclone                                                            && \
+	    libreoffice-help-ja libreoffice-l10n-ja                            && \
 	apt-get -q -y            autoremove                                    && \
 	apt-get -q               autoclean                                     && \
-	apt-get -q               clean
-	RETCD=$?
-	if [ ${RETCD} -ne 0 ]; then
-		fncEnd
-		exit 1
-	fi
+	apt-get -q               clean                                         || \
+	fncEnd $?
 # -- localize -----------------------------------------------------------------
-	pushd /usr/share/locale/ > /dev/null
-		rm -rf be* bg* cs* da* de* es* fi* fr* he* hi* hu* it* nl* pl* ru* sk* sl* tr* zh*
-	popd > /dev/null
-	localedef --list-archive | grep -v -e ^ja -e ^en_GB -e en_US | xargs localedef --delete-from-archive
-	sed -i /etc/xdg/lxsession/LXDE/autostart               \
-	    -e '$a@setxkbmap -layout jp -option ctrl:swapcase'
+	cat <<- _EOT_ >> /etc/xdg/lxsession/LXDE/autostart
+		@setxkbmap -layout jp -option ctrl:swapcase
+_EOT_
 # -- root user's setting ------------------------------------------------------
 	cat <<- _EOT_ > ~/.vimrc
 		set number
@@ -70,52 +66,25 @@ fncEnd() {
 		set list
 		set listchars=tab:>_
 _EOT_
-	cat <<- _EOT_ >> ~/.bashrc
-		#
-		case "\${TERM}" in
-		    "linux" )
-		        LANG=C
-		        ;;
-		    * )
-		        LANG=ja_JP.UTF-8
-		        ;;
-		esac
-		export LANG
+# -- knoppix user's setting ---------------------------------------------------
+	cat <<- _EOT_ > /home/knoppix/.vimrc
+		set number
+		set tabstop=4
+		set list
+		set listchars=tab:>_
+_EOT_
+	cat <<- _EOT_ >> /home/knoppix/.bashrc
 		export GTK_IM_MODULE=ibus
 		export XMODIFIERS=@im=ibus
 		export QT_IM_MODULE=ibus
 _EOT_
-# -- knoppix user's setting ---------------------------------------------------
-	cp -p ~/.vimrc  /home/knoppix/
-	cp -p ~/.bashrc /home/knoppix/
-	chown -R knoppix.knoppix /home/knoppix/.vimrc
-	chown -R knoppix.knoppix /home/knoppix/.bashrc
 # -- im-config ----------------------------------------------------------------
 # <memo>
 #   Change Kanji mode:[Windows key]+[Space key]->[Zenkaku/Hankaku key]
 	im-config -n ibus
 	mkdir /home/knoppix/.xinput.d
 	ln -s /etc/X11/xinit/xinput.d/ja_JP /home/knoppix/.xinput.d/ja_JP
-	chown -R knoppix.knoppix /home/knoppix/.xinput.d
-# -- clamav -------------------------------------------------------------------
-	sed -i /etc/clamav/freshclam.conf                                                            \
-	    -e 's/# Check for new database 24 times a day/# Check for new database 12 times a day/g' \
-	    -e 's/Checks 24/Checks 12/g'                                                             \
-	    -e 's/^NotifyClamd/#&/g'
-# -- sshd ---------------------------------------------------------------------
-	sed -i /etc/ssh/sshd_config                                         \
-	    -e 's/^PermitRootLogin .*/PermitRootLogin yes/g'                \
-	    -e 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' \
-	    -e '/HostKey \/etc\/ssh\/ssh_host_ecdsa_key/d'                  \
-	    -e '/HostKey \/etc\/ssh\/ssh_host_ed25519_key/d'                \
-	    -e '$aUseDNS no\nIgnoreUserKnownHosts no'
-# -- proftpd ------------------------------------------------------------------
-	sed -i /etc/proftpd/proftpd.conf                                                \
-	    -e '$a TimesGMT off\n<Global>\n\tRootLogin on\n\tUseFtpUsers on\n</Global>'
-	sed -i /etc/ftpusers      \
-	    -e 's/^root/# root/g'
+	chown -R knoppix:knoppix /home/knoppix/.xinput.d
 # -- cleaning -----------------------------------------------------------------
-	fncEnd
-	history -c
-	exit 0
+	fncEnd 0
 # -- EOF ----------------------------------------------------------------------
