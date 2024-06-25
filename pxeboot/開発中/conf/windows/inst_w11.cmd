f60752bbee344426d39f918fd3be8c0b860505b9
office-itou <itou-jun@outlook.com>
2024-06-25 16:43:48 +0900

    Update inst_w11.cmd

--- pxeboot/開発中/conf/windows/inst_w11.cmd
+++ pxeboot/開発中/conf/windows/inst_w11.cmd
@@ -1,5 +1,10 @@
     @Echo Off
 Rem SetLocal
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassTPMCheck        /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassCPUCheck        /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassRAMCheck        /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassStorageCheck    /t REG_DWORD /d 1 /f
     Wpeinit
     Set ShareName=\\sv-server\lhome\master\share\imgs\windows-11
     Echo Enter the name of the Windows shared folder where you extracted the installation media.
