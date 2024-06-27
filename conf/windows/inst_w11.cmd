10874ec4e863b45500788365e9d0b6c6ad0cd761
office-itou <itou-jun@outlook.com>
2024-06-27 09:45:24 +0900

    Creating an Automated Installation Configuration File

---
+++ conf/windows/inst_w11.cmd
@@ -0,0 +1,28 @@
+    @Echo Off
+Rem SetLocal
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassTPMCheck        /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassCPUCheck        /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassRAMCheck        /t REG_DWORD /d 1 /f
+    REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassStorageCheck    /t REG_DWORD /d 1 /f
+    Wpeinit
+    Set ShareName=\\sv-server\lhome\master\share\imgs\windows-11
+    Echo Enter the name of the Windows shared folder where you extracted the installation media.
+    Echo %ShareName%
+    Set /P ShareName=
+    Net Use %ShareName%
+    Set SetupExe=%ShareName%\setup.exe
+    Set AutoInst=%SystemDrive%\Windows\System32\unattend.xml
+    If Exist %SetupExe% (
+        If Exist %AutoInst% (
+            Echo Run %SetupExe% with %AutoInst%
+            %SetupExe% /Unattend:%AutoInst%
+        ) Else (
+            Echo Run %SetupExe%
+            %SetupExe%
+        )
+    ) Else (
+        Echo Missing %SetupExe%
+    )
+Rem EndLocal
+    Pause.
