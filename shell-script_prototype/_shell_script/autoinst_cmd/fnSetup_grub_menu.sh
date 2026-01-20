505ff8c64f6cc82e7cb36d3866b509e7bd053d67
office-itou <itou-jun@outlook.com>
2026-01-20 23:00:11 +0900

    Update functions

--- shell-script_prototype/_shell_script/autoinst_cmd/fnSetup_grub_menu.sh
+++ shell-script_prototype/_shell_script/autoinst_cmd/fnSetup_grub_menu.sh
@@ -12,56 +12,43 @@ fnSetup_grub_menu() {
 	__FUNC_NAME="fnSetup_grub_menu"
 	fnMsgout "${_PROG_NAME:-}" "start" "[${__FUNC_NAME}]"
 	# --- /etc/default/grub ---------------------------------------------------
-#	__NAME="${_DIST_NAME%"${_DIST_NAME#"${_DIST_NAME%%-*}"}"}"
-#	__VERS="${_DIST_VERS%"${_DIST_VERS#"${_DIST_VERS%%.*}"}"}"
-	__SLNX="$(fnFind_command "semanage")"
-	__APAR="$(fnFind_command "aa-enabled")"
+	__ENTR='
+'
 	__PATH="${_DIRS_TGET:-}/etc/default/grub"
-	__BOPT="$( \
-		sed -ne '/^GRUB_CMDLINE_LINUX_DEFAULT=/ {' \
-		    -e  's/^[^=]\+="//'                    \
-		    -e  's/[ "]*$//'                       \
-		    -e  's/ *security=[^ "]* *//'          \
-		    -e  's/ *apparmor=[^ "]* *//'          \
-		    -e  's/ *selinux=[^ "]* *//'           \
-		    -e  's/ *vga=[^ "]* *//'               \
-		    -e  'p}'                               \
-		    "${__PATH}"
-		)"
-	__WORK=""
+	__SLNX="$(fnFind_command "semanage")"	# selinux
+	__APAR="$(fnFind_command "aa-enabled")"	# apparmor
+	__BOPT=""
 	case "${_DIST_NAME:-}" in
 		debian|ubuntu)
-			  if [ -n "${__APAR:-}" ]; then __WORK="security=apparmor apparmor=1"
-			elif [ -n "${__SLNX:-}" ]; then __WORK="security=selinux selinux=1"
+			  if [ -n "${__APAR:-}" ]; then __BOPT="security=apparmor apparmor=1"
+			elif [ -n "${__SLNX:-}" ]; then __BOPT="security=selinux selinux=1"
 			fi
 			;;
 		fedora|centos|almalinux|rocky|miraclelinux)
-			  if [ -n "${__SLNX:-}" ]; then __WORK="security=selinux selinux=1"
-			elif [ -n "${__APAR:-}" ]; then __WORK="security=apparmor apparmor=1"
+			  if [ -n "${__SLNX:-}" ]; then __BOPT="security=selinux selinux=1"
+			elif [ -n "${__APAR:-}" ]; then __BOPT="security=apparmor apparmor=1"
 			fi
 			;;
 		opensuse-leap)
 			if [ "${_DIST_VERS%%.*}" -lt 16 ]; then
-				  if [ -n "${__APAR:-}" ]; then __WORK="security=apparmor apparmor=1"
-				elif [ -n "${__SLNX:-}" ]; then __WORK="security=selinux selinux=1"
+				  if [ -n "${__APAR:-}" ]; then __BOPT="security=apparmor apparmor=1"
+				elif [ -n "${__SLNX:-}" ]; then __BOPT="security=selinux selinux=1"
 				fi
 			else
-				  if [ -n "${__SLNX:-}" ]; then __WORK="security=selinux selinux=1"
-				elif [ -n "${__APAR:-}" ]; then __WORK="security=apparmor apparmor=1"
+				  if [ -n "${__SLNX:-}" ]; then __BOPT="security=selinux selinux=1"
+				elif [ -n "${__APAR:-}" ]; then __BOPT="security=apparmor apparmor=1"
 				fi
 			fi
 			;;
 		opensuse-tumbleweed)
-			  if [ -n "${__SLNX:-}" ]; then __WORK="security=selinux selinux=1"
-			elif [ -n "${__APAR:-}" ]; then __WORK="security=apparmor apparmor=1"
+			  if [ -n "${__SLNX:-}" ]; then __BOPT="security=selinux selinux=1"
+			elif [ -n "${__APAR:-}" ]; then __BOPT="security=apparmor apparmor=1"
 			fi
 			;;
 		*) ;;
 	esac
-	__BOPT="${__BOPT:+"${__BOPT} "}${__WORK:-}"
-	__BOPT="$(echo "${__BOPT:-}" | sed -e 's%/%\\/%g')"
-	__ENTR='
-'
+	# --- create /etc/default/grub.d/grub.cfg ---------------------------------
+	# https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
 	fnMsgout "${_PROG_NAME:-}" "info" "_DIST_NAME=[${_DIST_NAME:-}]"
 	fnMsgout "${_PROG_NAME:-}" "info" "    __BOPT=[${__BOPT:-}]"
 #	fnMsgout "${_PROG_NAME:-}" "info" "    __SLNX=[${__SLNX:-}]"
@@ -69,40 +56,43 @@ fnSetup_grub_menu() {
 	fnFile_backup "${__PATH}"			# backup original file
 	mkdir -p "${__PATH%/*}"
 	cp --preserve=timestamps "${_DIRS_ORIG}/${__PATH#*"${_DIRS_TGET:-}/"}" "${__PATH}"
-	if ! grep -q 'GRUB_CMDLINE_LINUX_DEFAULT' "${__PATH}"; then
-		sed -i "${__PATH}"                                                \
-		    -e '/^GRUB_CMDLINE_LINUX=.*$/i GRUB_CMDLINE_LINUX_DEFAULT=""'
-	fi
-	sed -i "${__PATH}"                                       \
-	    -e '/^#*GRUB_RECORDFAIL_TIMEOUT=.*$/              {' \
-	    -e 's/^#//                                         ' \
-	    -e 'h; s/^/#/; p; g; s/[0-9]\+$/10/                ' \
-	    -e '}                                              ' \
-	    -e '/^#*GRUB_TIMEOUT=.*$/                         {' \
-	    -e 's/^#//                                         ' \
-	    -e 'h; s/^/#/; p; g; s/[0-9]\+$/3/                 ' \
-	    -e '}                                              ' \
-	    -e '/^#*GRUB_CMDLINE_LINUX_DEFAULT=.*$/           {' \
-	    -e 's/^#//                                         ' \
-	    -e 'h; s/^/#/; p; g; s/=.*$/="'"${__BOPT:-}"'"/    ' \
-	    -e '}                                              ' \
-	    -e '/^#*GRUB_CMDLINE_LINUX=.*$/                   {' \
-	    -e 's/^#//                                         ' \
-	    -e 'h; s/^/#/; p; g;                               ' \
-	    -e 's/ *vga=[^ "]* *//'                              \
-	    -e '}                                              ' \
-	    -e '/^#*GRUB_GFXMODE=.*$/                         {' \
-	    -e 's/^#//                                         ' \
-	    -e 'h; s/^/#/; p; g;                               ' \
-	    -e 's/=.*$/="1920x1080x32,800x600x16,auto"/        ' \
-	    -e '}'
+#	if command -v grubby > /dev/null 2>&1; then
+#		grubby --update-kernel ALL --remove-args "security apparmor selinux vga" --args "${__BOPT}"
+#	else
+		__WORK="$(sed -ne 's/^GRUB_CMDLINE_LINUX=\(.*\)$/\1/p' "${__PATH}")"
+		__WORK="${__WORK#\"}"
+		__WORK="${__WORK%\"}"
+		__WORK="$(
+			echo "${__WORK:-}" | \
+			sed -e 's/ *security=[^ ]* *//' \
+			    -e 's/ *apparmor=[^ ]* *//' \
+			    -e 's/ *selinux=[^ ]* *// ' \
+			    -e 's/ *vga=[^ ]* *//'
+		)"
+		[ -n "${__WORK:-}" ] && __BOPT="${__WORK:+"${__WORK} ${__BOPT:-}"}"
+		__BOPT="$(echo "${__BOPT:-}" | sed -e 's%/%\\/%g')"
+		sed -i "${__PATH}"                                \
+		    -e '/^GRUB_RECORDFAIL_TIMEOUT=.*$/    s/^/#/' \
+		    -e '/^GRUB_TIMEOUT=.*$/               s/^/#/' \
+		    -e '/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/ s/^/#/' \
+		    -e '/^GRUB_CMDLINE_LINUX=.*$/         s/^/#/' \
+		    -e '/^GRUB_GFXMODE=.*$/               s/^/#/'
+		cat <<- _EOT_ | sed -e '/^ [^ ]\+/ s/^ *//g' -e 's/^ \+$//g' >> "${__PATH}"
+		
+			### User Custom ###
+			GRUB_RECORDFAIL_TIMEOUT="10"
+			GRUB_TIMEOUT="3"
+			GRUB_CMDLINE_LINUX="${__BOPT:-}"
+			GRUB_GFXMODE="1920x1080,800x600,auto"
+_EOT_
+#	fi
 	diff --suppress-common-lines --expand-tabs --side-by-side "${_DIRS_ORIG}/${__PATH#*"${_DIRS_TGET:-}/"}" "${__PATH}" || true
 	fnDbgdump "${__PATH}"				# debugout
 	fnFile_backup "${__PATH}" "init"	# backup initial file
-	# --- grub.cfg ------------------------------------------------------------
+	# --- grub.cfg --------------------------------------------------------
 	__PATH="$(find "${_DIRS_TGET:-}"/boot/ -ipath '/*/efi' -prune -o -name 'grub.cfg' -print)"
 	fnMsgout "${_PROG_NAME:-}" "create" "[${__PATH}]"
-	  if command -v grub-mkconfig > /dev/null 2>&1; then
+	if command -v grub-mkconfig > /dev/null 2>&1; then
 		grub-mkconfig --output "${__PATH:?}"
 	elif command -v grub2-mkconfig > /dev/null 2>&1; then
 		grub2-mkconfig --output "${__PATH:?}"
