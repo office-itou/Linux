cff145e0e7dc9938f1658690a67ec64feca72a8c
office-itou <itou-jun@outlook.com>
2026-02-07 02:52:31 +0900

    Update functions

--- shell-script_prototype/_shell_script/custom_cmd/fnRsync.sh
+++ shell-script_prototype/_shell_script/custom_cmd/fnRsync.sh
@@ -25,13 +25,15 @@ function fnRsync() {
 	if [[ ! -s "${__TGET_ISOS}" ]]; then
 		return
 	fi
-	printf "\033[m%-8s: %s\033[m\n" "rsync" "${__TGET_ISOS##*/}"
 	rm -rf "${__TEMP:?}"
 	mkdir -p "${__TEMP}" "${__TGET_DEST}"
 	mount -o ro,loop "${__TGET_ISOS}" "${__TEMP}"
 	__SRCS="$(LANG=C find "${__TEMP}"      -type d -prune -printf "%TY-%Tm-%Td%%20%TH:%TM:%TS%Tz")"
 	__DEST="$(LANG=C find "${__TGET_DEST}" -type d -prune -printf "%TY-%Tm-%Td%%20%TH:%TM:%TS%Tz")"
-	if [[ "${__SRCS:-}" != "${__DEST}" ]]; then
+	if [[ "${__SRCS:-}" = "${__DEST}" ]]; then
+		printf "\033[m%-8s: %s\033[m\n" "skip" "${__TGET_ISOS##*/}"
+	else
+		printf "\033[m%-8s: %s\033[m\n" "rsync" "${__TGET_ISOS##*/}"
 		nice -n "${_NICE_VALU:-19}" rsync "${_OPTN_RSYC[@]}" "${__TEMP}/." "${__TGET_DEST}/" 2>/dev/null || true
 	fi
 	umount "${__TEMP}"
